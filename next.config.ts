247dd7935142ebee8e6b6a88cc17d82dd5d99a3b
doan.le <117817950+doanla-kafi@users.noreply.github.com>
2025-02-15 15:20:03 +0700

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  /* config options here */
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+    NEXT_PUBLIC_DEFAULT_PROFILE_USERNAME:
+      process.env.NEXT_PUBLIC_DEFAULT_PROFILE_USERNAME || "default",
+  },
+  experimental: {
+    esmExternals: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
